f69aa70720e16a5ad45f9d9b88c07aa0a6e623a2
鬼鬼 <54203072+Akuma-real@users.noreply.github.com>
2024-10-14 15:02:02 +0000

    集成

--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -2,6 +2,22 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  vite: { 
+    optimizeDeps: {
+      exclude: [ 
+        '@nolebase/vitepress-plugin-enhanced-readabilities/client', 
+        'vitepress', 
+        '@nolebase/ui', 
+      ], 
+    },
+    ssr: { 
+      noExternal: [ 
+        // 如果还有别的依赖需要添加的话，并排填写和配置到这里即可
+        '@nolebase/vitepress-plugin-enhanced-readabilities', 
+        '@nolebase/ui', 
+      ], 
+    }, 
+  }, 
   title: "鬼鬼Samaの小窝",
   description: "这里放着我的一些小破烂。🌍",
   themeConfig: {
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,6 +2,13 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import type { Options } from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+import { InjectionKey } from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+import { 
+  NolebaseEnhancedReadabilitiesMenu, 
+  NolebaseEnhancedReadabilitiesScreenMenu, 
+} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
 import './style.css'
 
 export default {
@@ -9,9 +16,29 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      // 为较宽的屏幕的导航栏添加阅读增强菜单
+      'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu), 
+      // 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
+      'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu),       
     })
   },
   enhanceApp({ app, router, siteData }) {
     // ...
+    app.provide(InjectionKey, { 
+      // 配置...
+      locales: { // 配置国际化
+        'zh-CN': { // 配置简体中文
+          title: { 
+            title: '阅读增强插件', 
+          } 
+        }, 
+        'en': { // 配置英文
+          title: { 
+            title: 'Enhanced Readabilities Plugin', 
+          } 
+        } 
+      }       
+    } as Options)   
   }
+
 } satisfies Theme
